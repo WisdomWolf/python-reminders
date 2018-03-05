d6fd1dc14e7eb381fc55c0cdd0d3baf062f757fc
WisdomWolf <wisdomwolf@gmail.com>
2018-03-05 08:20:10 -0600

    updated setup

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
+
 setup(name='python-reminders',
       version='0.1',
       py_modules=['reminders'],
-      )
+      author='@wisdomwolf',
+      author_email='wisdomwolf@gmail.com',
+      install_requires=[
+          'APScheduler>=3.5.1',
+          'requests>=2.3.0'
+      ],
+)
