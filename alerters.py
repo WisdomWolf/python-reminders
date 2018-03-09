c8b98628b95f661d7cfca884c624c174e4c021e6
WisdomWolf <wisdomwolf@gmail.com>
2018-03-09 11:56:17 -0600

    added docstrings to alerters.py

--- alerters.py
+++ alerters.py
@@ -1,8 +1,21 @@
 import logging
 
 class Alerter(object):
-    
+    """Base Alert object to handle reminder notifications."""
+
     def __init__(self, reminder, message, notifiers=None, repeat_args=None, *args, **kwargs):
+        """
+        Create Alerter object.
+
+        :param Reminder reminder: Reminder instance to associate this alert with.
+        :param str message:
+            Message to be sent by notifier(s).
+            note: This was added as part of POC. Likely to be removed in future.
+        :param notifiers: ¯\_(ツ)_/¯
+        :param dict repeat_args:
+            Arguments to set repeat interval and max number of repeats.
+            note: Might make more sense to seperate out into individual arguments.
+        """
         self.logger = logging.getLogger(__name__)
         self.message = message
         
